b09e0592c1f9c1e4bfff8617de0390506644ac51
vedvyas9 <vedvyas9@gmail.com>
2020-11-23 18:11:07 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import hashlib
+
+flag=0
+counter=0
+
+pass_hash=input("Enter md5 hash: ")
+wordlist=input("File name: ")
+
+try:
+    pass_file=open(wordlist,"r")
+except:
+    print('No file found')
+    quit()
+
+for word in pass_file:
+    enc_wrd=word.encode('utf-8')
+    digest=hashlib.md5(enc_wrd.strip()).hexdigest()
+
+
+    print(word)
+    print(digest)
+    print(pass_hash)
+    counter+=1
+
+    if digest==pass_hash:
+        print("Password found")
+        print("Password is " +  word)
+        print(counter)
+        flag=1
+        break
+
+if flag==0:
+    print("password is not on the list")
+    print(counter)
